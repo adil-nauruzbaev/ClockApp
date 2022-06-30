bde959209cfe3310fc19ff03cc1c61aa465d9eb5
DieonN <aresbitch@mail.ru>
2022-06-30 10:36:49 +0600

    svg

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('sada')
+        leading: IconButton(
+          icon: SvgPicture.asset('assets/icons/Settings.svg'),
+          // icon: Image.asset('assets/icons/setting.png'),
+          onPressed: () {},
+        ),
       ),
     );
   }
