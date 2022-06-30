cbb680a063bbd4c83493a7ecea66c8994af8db83
DieonN <aresbitch@mail.ru>
2022-06-30 16:22:20 +0600

    clock

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Clock',
       theme: themeData(context),
       darkTheme: darkThemeData(context),
-      themeMode: ThemeMode.dark,
+      themeMode: ThemeMode.light,
       home: HomeScreen(),
     );
   }
--- lib/screens/companents/body.dart
+++ lib/screens/companents/body.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget {
@@ -20,6 +22,12 @@ class Body extends StatelessWidget {
   }
 }
 
+var today = new DateTime.now();
+var fiftyDaysFromNow = today.add(new Duration(hours: 1));
+void fif() {
+  print(fiftyDaysFromNow);
+}
+
 class TimeInYearsHourAndMinute extends StatefulWidget {
   const TimeInYearsHourAndMinute({Key key}) : super(key: key);
 
@@ -31,11 +39,28 @@ class TimeInYearsHourAndMinute extends StatefulWidget {
 class _TimeInYearsHourAndMinuteState extends State<TimeInYearsHourAndMinute> {
   TimeOfDay _timeOfDay = TimeOfDay.now();
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer.periodic(Duration(seconds: 1),(timer) {
+      if(_timeOfDay.minute != TimeOfDay.now().minute){
+        setState(() {
+          _timeOfDay = TimeOfDay.now();
+        });
+      }
+    });
+  }
+  @override
   Widget build(BuildContext context) {
-    String _reriod = _timeOfDay.period == DayPeriod.am ? "AM" : "PM";
+    String _period = _timeOfDay.period == DayPeriod.am ? "AM" : "PM";
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: [Text("${_timeOfDay.hour}", style: Theme.of(context).textTheme.headline1,)],
+      children: [
+        Text(
+          "${_timeOfDay.hour}:${_timeOfDay.minute}",
+          style: Theme.of(context).textTheme.headline1,
+        )
+      ],
     );
   }
 }
